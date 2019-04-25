5ef576ef39cf61c9e5769ea13f0a79046663bcd3
Mikaela Pisani <mpisani@netlabs.com.uy>
2019-04-24 23:45:23 -0500

    add code for mds 3plot

--- Test_1.R
+++ Test_1.R
@@ -206,20 +206,7 @@ for(q in 1:ncol(data)){
 }
 
 
-#MDS
-
-mydata <- data[, c(-1,-2)]
-data.s <- scale(mydata)
-d <- dist(data.s)
-cmd <- cmdscale(d, k= 5, eig = T)
-cumsum(cmd$eig[1:5])/sum(cmd$eig[1:5]) #we choose 3 because cover 78%
-
-cmd <- cmdscale(dist(data.s), k= 3)
-cmd
-
-library("scatterplot3d")
-s3d <- scatterplot3d(cmd, pch = "", cex.axis = 1)
-
+#MDS by continent
 by_continent <- aggregate(data[, 3:17], list(data$continent), mean)
 cmd_continent <- cmdscale(dist(scale(by_continent[, c(-1,-2)])), k=5, eig = T)
 cumsum(cmd_continent$eig[1:5])/sum(cmd_continent$eig[1:5]) 
@@ -228,3 +215,16 @@ colors <- c("#999999", "#E69F00", "#56B4E9", "#483D8B", "#8B0000", "#C71585", "#
 plot(cmd_continent, xlab = "coordinate 1", ylab = "coordinate 2", pch=19, col=colors) 
 legend(-2.5, -2, fill=colors, legend=by_continent$Group.1, col=colors, cex=0.8)
 
+#MD5
+data.s <- scale(data[, c(-1,-2)])
+d <- dist(data.s)       
+cmd <- cmdscale(d, k=5, eig = T)
+cumsum(cmd$eig[1:5])/sum(cmd$eig[1:5]) 
+cmd <- cmdscale(d, k=3)
+library(scatterplot3d)
+
+#True clusters: continents
+colors_data <- colors[as.numeric(factor(data$continent))]
+s3d <- scatterplot3d(cmd, pch=16, cex.axis = 1, color = colors_data)
+legend("topright", fill=colors_data, legend=levels(factor(data$continent)), col=colors_data, cex=0.8)
+?legend
