5bb1b85763862687f96305f5d6b53a8c067a2492
Mikaela Pisani <mpisani@netlabs.com.uy>
2019-04-25 23:02:19 -0500

    add Qatar

--- MDS.R
+++ MDS.R
@@ -29,6 +29,3 @@ plot(cmd_continent, xlab = "coordinate 1", ylab = "coordinate 2", pch=16, col=co
 legend(-2.5, -1, fill=colors, legend=by_continent$Group.1, col=colors, cex=0.8)
 
 
-
-
-install.packages(c("rgl", "car"))
