59398f751466a1e5b5fc6633f0d7299d54f013e4
grzechowiak <grzechubjj@gmail.com>
2019-04-25 19:14:54 -0500

    different scripts

--- PCA.R
+++ PCA.R
@@ -1,5 +1,4 @@
 data2 <- data
-library(dplyr)
 colnames(data2)
 data2 <- select(data,'continent', 'country', #'armed_pp',
                 'phones_p100','children_p_woman','life_exp_yrs',
@@ -23,14 +22,6 @@ biplot(pca,xlabs=abbreviate(data2$continent))
 biplot(prcomp(data2[,c(-1,-2)]), choices=c(1,2),xlabs=(data2$country))     ## plots ax1 and ax2 
 
 
-## PC1 can be high develope countries
-# because not many children_p_woman, suicide is high
-### REMEMBER suicide_pp, child_mort_p1000 and gini are REVERSED
-
-## PC2 Arabic country:
-#more inequality
-# Suicide is very low
-#gini is high (more inequality)
 
 #Country with low comp2 
 
@@ -40,16 +31,22 @@ biplot(prcomp(data2[,c(-1,-2)]), choices=c(1,2),xlabs=(data2$country))     ## pl
 #Filter what to display on biplot
 
 
-library(devtools)
-install_github("vqv/ggbiplot")
+#library(devtools)
+#install_github("vqv/ggbiplot")
+
 
 
 
 library(ggbiplot)
 wine.pca <- prcomp(data2[,c(-1,-2)], scale. = TRUE)
 ggbiplot(wine.pca, obs.scale = 1, var.scale = 1,
-         groups = data2$continent, ellipse = TRUE, circle = TRUE) +
-  scale_color_discrete(name = '') +
+         groups = data2$continent, ellipse = TRUE) +
+  scale_color_discrete(name = 'Continents:') +
   theme(legend.direction = 'horizontal', legend.position = 'top')
 
 #https://github.com/vqv/ggbiplot
+
+
+data(wine)
+wine.pca <- prcomp(wine, scale. = TRUE)
+print(ggbiplot(wine.pca, obs.scale = 1, var.scale = 1, groups = wine.class, ellipse = TRUE))
