7e7c70f88a07ce83c86e834cd5f8b305e91e3cf3
grzechowiak <grzechubjj@gmail.com>
2019-04-26 18:56:19 -0500

    groups on the map

--- clusters_countries.R
+++ clusters_countries.R
@@ -4,9 +4,12 @@
 
 #Copy data
 clus_coun <- data
+row.names(clus_coun) <- clus_coun$country
+#delete columns: country, continent
+clus_coun[c(1,2)] <- NULL
 
-#Save without names and scale
-clus_coun.s <- scale(clus_coun[,c(-1,-2)])
+#Scale data
+clus_coun <- scale(clus_coun)
 
 #Function to check # of clusters in a data
 plot.wgss = function(mydata, maxc) {
@@ -18,11 +21,22 @@ plot.wgss = function(mydata, maxc) {
 }
 
 #Check # of cluster
-plot.wgss(clus_coun.s, 20) 
+plot.wgss(clus_coun, 20)
 # Plots shows around 7 clausters
 
-km <- kmeans(clus_coun.s, centers=7)
-
+#Perform clustering
+km <- kmeans(clus_coun, centers=6, nstart = 20)
 table(km$cluster)
 
-subset(clus_coun.s, km$cluster == 1)
+#### GROUP 1 #### 
+gr1 <- as.data.frame(subset(clus_coun, km$cluster == 1))
+gr1$country <- rownames(gr1)
+gr1 <- as.vector(unlist(gr1$country))
+
+#Plot group 1
+library(maptools)
+data(wrld_simpl)
+myCountries = wrld_simpl@data$NAME %in% gr1
+plot(wrld_simpl, col = c(gray(.80), "red")[myCountries+1], main='Group 1')
+
+
