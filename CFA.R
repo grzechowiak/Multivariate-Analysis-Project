19b5bed158deb40bcf23dfa1f20798c921a975c4
Mikaela Pisani <mpisani@netlabs.com.uy>
2019-04-27 16:09:43 -0500

    add Function to EFA

--- CFA.R
+++ CFA.R
@@ -6,11 +6,17 @@
 #####with 2 factors
 library(sem)
 cfa_data <- cleaned
-cfa_data <- cleaned
+colnames(cfa_data)
 cfa_data <- cfa_data[,c(-1,-2,-4,-5,-9,-11,-15,-16,-17)]
-cfa_model <- specifyModel(file = "2_factors.txt")
+
+cfa_data <- cfa_data[,c(-1,-2,-9,-17)]
+cfa_model <- specifyModel(file = "cfa_model.txt")
 colnames(cfa_data)
 cfa_sem <- sem(cfa_model, cor(cfa_data), nrow(cfa_data))
 summary(cfa_sem)
-##########
 
+cfa_result <- cfa(file = "cfa_model.txt")
+fit <- cfa(file = "cfa_model.txt", sample.cov=cor(cfa_data), sample.nobs=nrow(cfa_data),  std.lv=FALSE)
+
+
+summary(cfa_result)
