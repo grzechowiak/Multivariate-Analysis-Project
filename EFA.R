e237430acb663ab47f91b85aaac5010186d7715e
Mikaela Pisani <mpisani@netlabs.com.uy>
2019-04-26 19:57:24 -0500

    add graph countries

--- EFA.R
+++ EFA.R
@@ -15,9 +15,32 @@ factor_rms(num_data, 2, corr)
 factor_rms(num_data, 3, corr)
 factor_rms(num_data, 4, corr)
 
-data.fa <- factanal(num_data, factors = 4)
+data.fa <- factanal(num_data, factors = 4, scores = "regression")
 data.fa$loadings
 print(data.fa$loadings, cut=0.4)
+scores <- data.fa$scores
+scores <- as.data.frame(scores)
+row.names(scores) <- data$country
 
+### GROUP 1 #### 
+gr1 <- tail(scores[order(scores$Factor1),],5)
+gr1$country <- rownames(gr1)
+gr1 <- as.vector(unlist(gr1$country))
+gr2 <- tail(scores[order(scores$Factor2),],5)
+gr2$country <- rownames(gr2)
+gr2 <- as.vector(unlist(gr2$country))
+
+#Plot group 1
+library(maptools)
+data(wrld_simpl)
+
+country_colors <- setNames(rep("white", length(wrld_simpl@data$NAME)), wrld_simpl@data$NAME)
+country_colors[wrld_simpl@data$NAME %in% gr1] <- "red"
+country_colors[wrld_simpl@data$NAME %in% gr2] <- "blue"
+country_colors[wrld_simpl@data$NAME %in% gr3] <- "green"
+country_colors[wrld_simpl@data$NAME %in% gr4] <- "yellow"
+
+sort(wrld_simpl@data$NAME)
+plot(wrld_simpl, col = country_colors)
 
 
