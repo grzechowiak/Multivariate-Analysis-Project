16cc2ceb1b452d79d30c3dca5cdb82d454e13035
grzechowiak <grzechubjj@gmail.com>
2019-04-27 15:58:45 -0500

    Correction

--- Read_Clean.R
+++ Read_Clean.R
@@ -208,72 +208,72 @@ for(i in 1:length(col_names)){
 ##Check by Boxplots
 
 library(car)
-windows(); par(mfrow=c(3,5))
+par(mfrow=c(3,5))
 
 #Pop_Total Column
-with(cleaned, Boxplot(pop_total, id=list(labels=cleaned$country),main="Population Total"))
+with(data, Boxplot(pop_total, id=list(labels=data$country),main="Population Total"))
 
 #Murder_PP Column
-with(cleaned, Boxplot(murder_pp, id=list(labels=cleaned$country),main="Murder Per Person"))
+with(data, Boxplot(murder_pp, id=list(labels=data$country),main="Murder Per Person"))
 
 #Armed_PP Column
-with(cleaned, Boxplot(armed_pp, id=list(labels=cleaned$country),main="Armed Per Person"))
+with(data, Boxplot(armed_pp, id=list(labels=data$country),main="Armed Per Person"))
 
 #Phones_p100 Column
-with(cleaned, Boxplot(phones_p100, id=list(labels=cleaned$country),main="Phones Per 100 People"))
+with(data, Boxplot(phones_p100, id=list(labels=data$country),main="Phones Per 100 People"))
 
 #children_p_woman Column
-with(cleaned, Boxplot(children_p_woman, id=list(labels=cleaned$country),main="Children Per Woman"))
+with(data, Boxplot(children_p_woman, id=list(labels=data$country),main="Children Per Woman"))
 
 #Life_Exp_Yrs Column
-with(cleaned, Boxplot(life_exp_yrs, id=list(labels=cleaned$country),main="Life Expectancy in Years"))
+with(data, Boxplot(life_exp_yrs, id=list(labels=data$country),main="Life Expectancy in Years"))
 
 #Suicide_PP Column
-with(cleaned, Boxplot(suicide_pp, id=list(labels=cleaned$country),main="Suicide Per Person"))
+with(data, Boxplot(suicide_pp, id=list(labels=data$country),main="Suicide Per Person"))
 
 #Urban_Pop_Tot Column
-with(cleaned, Boxplot(urban_pop_tot, id=list(labels=cleaned$country),main="Urban Population Total"))
+with(data, Boxplot(urban_pop_tot, id=list(labels=data$country),main="Urban Population Total"))
 
 #Sex_Ratio_p100
-with(cleaned, Boxplot(sex_ratio_p100, id=list(labels=cleaned$country),main="Sex Ratio Per 100 People"))
+with(data, Boxplot(sex_ratio_p100, id=list(labels=data$country),main="Sex Ratio Per 100 People"))
 
 #Corruption_CPI
-with(cleaned, Boxplot(corruption_CPI, id=list(labels=cleaned$country),main="Corruption CPI Index"))
+with(data, Boxplot(corruption_CPI, id=list(labels=data$country),main="Corruption CPI Index"))
 
 #Internet_%0f_pop
-with(cleaned, Boxplot(`internet_%of_pop`, id=list(labels=cleaned$country),main="Internet Usage Perceantage of Population"))
+with(data, Boxplot(`internet_%of_pop`, id=list(labels=data$country),main="Internet Usage Perceantage of Population"))
 
 #child_mort_p1000
-with(cleaned, Boxplot(child_mort_p1000, id=list(labels=cleaned$country),main="Child Mortality Rate per 1000"))
+with(data, Boxplot(child_mort_p1000, id=list(labels=data$country),main="Child Mortality Rate per 1000"))
 
 #income_per_person
-with(cleaned, Boxplot(income_per_person, id=list(labels=cleaned$country),main="Income Per Person"))
+with(data, Boxplot(income_per_person, id=list(labels=data$country),main="Income Per Person"))
 
 #investments_per_ofGDP
-with(cleaned, Boxplot(investments_per_ofGDP, id=list(labels=cleaned$country),main="Investments Percentage of GDP"))
+with(data, Boxplot(investments_per_ofGDP, id=list(labels=data$country),main="Investments Percentage of GDP"))
 
 #Gini
-with(cleaned, Boxplot(gini, id=list(labels=cleaned$country),main="GINI Ratio"))
+with(data, Boxplot(gini, id=list(labels=data$country),main="GINI Ratio"))
 
 
 
 ## Check by Histograms
 
-hist(cleaned$pop_total)
-hist(cleaned$murder_pp)
-hist(cleaned$armed_pp)
-hist(cleaned$phones_p100)
-hist(cleaned$children_p_woman)
-hist(cleaned$life_exp_yrs)
-hist(cleaned$suicide_pp)
-hist(cleaned$urban_pop_tot)
-hist(cleaned$sex_ratio_p100)
-hist(cleaned$corruption_CPI)
-hist(cleaned$`internet_%of_pop`)
-hist(cleaned$child_mort_p1000)
-hist(cleaned$income_per_person)
-hist(cleaned$investments_per_ofGDP)
-hist(cleaned$gini)
+hist(data$pop_total)
+hist(data$murder_pp)
+hist(data$armed_pp)
+hist(data$phones_p100)
+hist(data$children_p_woman)
+hist(data$life_exp_yrs)
+hist(data$suicide_pp)
+hist(data$urban_pop_tot)
+hist(data$sex_ratio_p100)
+hist(data$corruption_CPI)
+hist(data$`internet_%of_pop`)
+hist(data$child_mort_p1000)
+hist(data$income_per_person)
+hist(data$investments_per_ofGDP)
+hist(data$gini)
 
 
 
